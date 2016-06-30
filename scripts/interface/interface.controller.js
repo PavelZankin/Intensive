0867cc7c6fc286f278d950abac5441f7a2119639
Pavel Zankin <Littlemooch73@mail.ru>
2016-06-30 18:56:49 +0400

    correcting
    little changes

--- scripts/interface/interface.controller.js
+++ scripts/interface/interface.controller.js
@@ -28,13 +28,13 @@ $('#moreSearch').on('click', function(){
 
 /* переключение вкладок между соискателем и работодателем*/
 
-$('#employer').on('click', function(){
-    $('#employer').css('background','#fff');
-    $('#applicant').css('background','lightgray')
+$('.employer').on('click', function(){
+    $('.employer').css('background','#fff');
+    $('.applicant').css('background','lightgray')
 });
-$('#applicant').on('click', function(){
-    $('#applicant').css('background','#fff');
-    $('#employer').css('background','lightgray')
+$('.applicant').on('click', function(){
+    $('.applicant').css('background','#fff');
+    $('.employer').css('background','lightgray')
 });
 
 /* раскрывающиеся div с вакансией */
