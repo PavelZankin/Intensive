b2211caae9cd77d6cddbf745c8e4d2cb012c685d
Pavel Zankin <Littlemooch73@mail.ru>
2016-07-03 13:20:35 +0400

    favorites
    add module favorites

--- scripts/search/search.view.js
+++ scripts/search/search.view.js
@@ -1,27 +1,19 @@
-var templ = `
+﻿var templ = `
   <% vacs.forEach(function(vac) { %>
     <div class="job">
+
+    <img class="star" src="../images/star.png" alt="" />
+
     <h3><%- vac.name %></h3>
 
-    <span><%- vac.area.name %></span><br/>
-
-    <a href=<%- vac.alternate_url %> >(Посмотреть на hh.ru)</a>
-
-    <div class="requirement">
-      <%= vac.snippet.requirement %>
-    </div>
-
-    <br/>
-
-    <div class="responsibility">
-      <%= vac.snippet.responsibility %>
-    </div>
+    <a href=<%- vac.alternate_url %> >("Посмотреть на hh.ru")</a>
 
     <br/>
 
     <span class="date"><%- vac.created_at %></span>
 
     <hr/>
+    </div>
   <% }); %>
 `;
 
@@ -42,6 +34,21 @@ function makeSearch() {
     $('#list-of-results')[0].innerHTML = _.template(templ)({
       vacs: response.items,
     });
+
+    $('.employer').css('background','#fff');
+    $('.applicant').css('background','lightgray')
+
+    if ($('#list-of-results').hasClass('hidden')) {
+      $('#list-of-results').removeClass('hidden');
+    } 
+
+    $('.star').on('click', function() {
+      if ($(this, '.star').parent().hasClass('favorites')) {
+        $(this, '.star').parent().removeClass('favorites');
+      }
+      else $(this, '.star').parent().addClass('favorites');
+    })
+
 
   });
 }
